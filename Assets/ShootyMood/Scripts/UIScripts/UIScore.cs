a130e0b4d5a00f17f781e49980fc149b1c159a7e
Cag <chaylar@gmail.com>
2023-07-28 19:42:49 +0300

    ui effect

--- Assets/ShootyMood/Scripts/UIScripts/UIScore.cs
+++ Assets/ShootyMood/Scripts/UIScripts/UIScore.cs
@@ -3,19 +3,34 @@ using ShootyMood.Scripts.Config.Wave;
 using ShootyMood.Scripts.ShootyGameEvents;
 using System;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
+using static UnityEngine.GraphicsBuffer;
 
 namespace Assets.ShootyMood.Scripts.UIScripts
 {
     public class UIScore : MonoBehaviour, IInitializable, IDisposable
     {
         [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private GameObject effectPostion;
+        [SerializeField] private GameObject uiParticle;
         [Inject] private WavesConfig wavesConfig;
         [Inject] private SignalBus signalBus;
 
+        //private Vector3 uiParticleStartingScale;
+
         private int score = 0;
+
+        private void OnDisable()
+        {
+            if (uiParticle != null)
+            {
+                uiParticle.gameObject.SetActive(false);
+            }
+        }
 
         public void Initialize()
         {
@@ -44,6 +59,18 @@ namespace Assets.ShootyMood.Scripts.UIScripts
 
             score = score < 0 ? 0 : score;
             scoreText.text = score.ToString();
+
+            if (score % 20 == 0)
+            {
+                InstantiateParticle();
+            }
+        }
+
+        private void InstantiateParticle()
+        {
+            var uiPart = Instantiate(uiParticle);
+            var starPosition = Camera.main.ScreenToWorldPoint(effectPostion.transform.position);
+            uiPart.transform.position = new Vector3(starPosition.x, starPosition.y, 0f);
         }
 
         private void OnGameStarted(GameStarted evt)
